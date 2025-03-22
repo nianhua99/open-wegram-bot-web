7ceca4c1cb11d34a63283c9f1ffcd0f23107273b
nianhua99 <1139168548@qq.com>
2025-03-22 22:47:43 +0800

    Vercel KV 使用 @upstash/redis 库

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@
  */
 
 import {handleRequest} from '../src/core.js';
+import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
     const request = new Request(`${req.headers['x-forwarded-proto']}://${req.headers.host}${req.url}`, {
@@ -22,20 +23,15 @@ export default async function handler(req, res) {
     const storage = {
         // Vercel KV 使用 @upstash/redis 库
         async get(key) {
-            const { kv } = require('@vercel/kv');
             return await kv.get(key);
         },
         async put(key, value) {
-            const { kv } = require('@vercel/kv');
             await kv.set(key, value);
         },
         async remove(key) {
-            const { kv } = require('@vercel/kv');
             await kv.delete(key);
         },
         async list(options){
-            const { kv } = require('@vercel/kv');
-            // 根据 options.prefix 过滤列表
             const list = await kv.list(options);
             return list.filter(item => item.name.startsWith(options.prefix));
         }
